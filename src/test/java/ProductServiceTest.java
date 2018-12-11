2e24f865a56b35feda7741fcf74b0acf186456e3
luyahui <luyh1090169180@gmail.com>
2018-12-10 22:15:48 -0500

    Enabled CORS.

--- src/test/java/ProductServiceTest.java
+++ src/test/java/ProductServiceTest.java
@@ -38,25 +38,37 @@ public class ProductServiceTest {
 //        p.setImageUrl("https://dimg05.c-ctrip.com/images/100a0c00000064pqzDAB6_C_238_268.jpg");
 //        productService.save(p);
 
-//        // 紫砂壶3
-//        p.setName("紫砂壶3");
-//        p.setImageUrl("https://youimg1.c-ctrip.com/target/10080j0000009zuyqADB9_R_300_300_Q90.jpg?proc=autoorient");
-//        productService.save(p);
+        // 紫砂壶3
+        p.setName("紫砂壶3");
+        p.setImageUrl("https://youimg1.c-ctrip.com/target/10080j0000009zuyqADB9_R_300_300_Q90.jpg?proc=autoorient");
+        productService.save(p);
 
 
-//        // 紫砂壶4
-//        p.setName("紫砂壶4");
-//        p.setImageUrl("https://dimg02.c-ctrip.com/images/fd/tg/g2/M05/87/D9/Cghzf1WwsVWAFjE2ACqTKYbSfPQ625_D_220_150.jpg");
-//        productService.save(p);
+        // 紫砂壶4
+        p = new Product();
+        p.setAuthor(author);
+        p.setMaterial("紫砂");
+        p.setType("方器");
+        p.setName("紫砂壶4");
+        p.setImageUrl("https://dimg02.c-ctrip.com/images/fd/tg/g2/M05/87/D9/Cghzf1WwsVWAFjE2ACqTKYbSfPQ625_D_220_150.jpg");
+        productService.save(p);
 
         // 紫砂壶5
+        p = new Product();
+        p.setAuthor(author);
+        p.setMaterial("紫砂");
+        p.setType("方器");
         p.setName("紫砂壶5");
         p.setImageUrl("https://dimg02.c-ctrip.com/images/fd/tg/g2/M05/87/D9/Cghzf1WwsVWAFjE2ACqTKYbSfPQ625_D_220_150.jpg");
         productService.save(p);
 
-//        // 紫砂壶6
-//        p.setName("紫砂壶6");
-//        p.setImageUrl("https://dimg02.c-ctrip.com/images/fd/tg/g2/M05/87/D9/Cghzf1WwsVWAFjE2ACqTKYbSfPQ625_D_220_150.jpg");
-//        productService.save(p);
+        // 紫砂壶6
+        p = new Product();
+        p.setAuthor(author);
+        p.setMaterial("紫砂");
+        p.setType("方器");
+        p.setName("紫砂壶6");
+        p.setImageUrl("https://dimg02.c-ctrip.com/images/fd/tg/g2/M05/87/D9/Cghzf1WwsVWAFjE2ACqTKYbSfPQ625_D_220_150.jpg");
+        productService.save(p);
     }
 }
